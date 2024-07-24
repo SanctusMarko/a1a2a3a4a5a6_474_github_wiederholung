af34c8e39bb0ecdf5af93bd25212bdac90efc952
SanctusMarko <fortalezaosijek@gmail.com>
2024-07-24 11:33:40 +0200

    jurgen<jurgenovich

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('BLA BLA First'),
+          child: Text('BLA Jurgen < Jurgenovich First'),
         ),
       ),
     );
