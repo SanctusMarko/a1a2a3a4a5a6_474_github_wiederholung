39d8d980b8743292441982317bcc44288ebe08fc
Hoovey@gmx.de <hoovey@gmx.de>
2024-07-24 11:17:00 +0200

    BLABLA_First

---
+++ README.md
@@ -0,0 +1,3 @@
+# a1a2a3a4a5a6_474_github_wiederholung
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/a1a2a3a4a5a6_474_github_wiederholung/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.a1a2a3a4a5a6_474_github_wiederholung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
